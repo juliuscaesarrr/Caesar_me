949121c767ca4781a330a18f89124c597be13b6f
Julius Caesar <souumyya@gmail.com>
2024-06-22 12:01:44 +0530

    6/22/24
    Signed-off-by: Julius Caesar <souumyya@gmail.com>

--- src/Lectures/DSA/cycleSort/cycleSort.java
+++ src/Lectures/DSA/cycleSort/cycleSort.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 // when given number from 1 to n than use "cycle sort";
 public class cycleSort {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 6, 3, 1, 2};
+        int[] arr = {5, 4, 6, 1, 2};
         int p = 0;
         while (p < arr.length) {
-            if (arr[p] != (p + 1)) {
+           if (arr[p] != (p + 1)) {
                 int k = arr[p];
                 arr[p] = arr[k - 1];
                 arr[k - 1] = k;
@@ -14,8 +14,9 @@ public class cycleSort {
             p++;
         }
         }
-        for(int a : arr){
+        for(int a : arr) {
             System.out.print(a + " ");
         }
     }
 }
+
